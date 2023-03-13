9afea253d0d47b8d157855c22c915244d0ff9c5e
Kevin Walker <kevin.walker@cloudenterprise.co.uk>
2023-03-13 15:26:05 +0000

    End of Section 4

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,11 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
 import { Header } from 'semantic-ui-react';
 import List from 'semantic-ui-react/dist/commonjs/elements/List';
 import ListItem from 'semantic-ui-react/dist/commonjs/elements/List/ListItem';
-import Button from 'semantic-ui-react/dist/commonjs/elements/Button';
 
 function App() {
 
